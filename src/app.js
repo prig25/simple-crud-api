155aea4501a92222b5ad2fdc9f893f23eeae506e
prig25 <5446068@gmail.com>
2021-11-27 20:16:24 +0300

    feat: create server

---
+++ src/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const http = require('http');
+
+const router = require('./router');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((request, response) => {
+
+  router(request, response);
+
+}).listen(PORT);
+
+console.log(`Server has been started on http://localhost:${PORT} ...`);
