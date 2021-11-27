ee76fc755f6d60fea8b663d5acaa6fef7143be4f
prig25 <5446068@gmail.com>
2021-11-27 20:19:07 +0300

    feat: implement error handler

---
+++ src/utils/errorHandler.js
@@ -0,0 +1,52 @@
+const { ERROR } = require('../const');
+
+function errorHandler(response, errorCode) {
+  response.setHeader("Content-Type", "application/json");
+
+  switch (errorCode) {
+    case ERROR.PERSON_ID_NOT_VALID:
+      response.statusCode = 400;
+      response.end(JSON.stringify({
+        message: "Person id not valid..."
+      }));
+      break;
+    case ERROR.REQUIRED_FIELDS:
+      response.statusCode = 400;
+      response.end(JSON.stringify({
+        message: "The request body does not contain required fields..."
+      }));
+      break;
+    case ERROR.ENDPOINT_NOT_EXIST:
+      response.statusCode = 404;
+      response.end(JSON.stringify({
+        message: "Resource that you requested doesn't exist..."
+      }));
+      break;
+    case ERROR.NOT_FOUND:
+      response.statusCode = 404;
+      response.end(JSON.stringify({
+        message: "Not found..."
+      }));
+      break;
+    case ERROR.PERSON_NOT_FOUND:
+      response.statusCode = 404;
+      response.end(JSON.stringify({
+        message: "Person not found..."
+      }));
+      break;
+    case ERROR.UNKNOWN:
+      response.statusCode = 500;
+      response.end(JSON.stringify({
+        message: `Error with code ${response.statusCode}...`
+      }));
+      break;
+    default:
+      response.statusCode = 500;
+      response.end(JSON.stringify({
+        message: `Error with code ${response.statusCode}...`
+      }));
+      break;
+  }
+}
+
+module.exports = errorHandler;
