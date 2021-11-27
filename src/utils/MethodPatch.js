31faf7e688ec6432ab18f6db9f74487fafa0a722
prig25 <5446068@gmail.com>
2021-11-27 20:20:21 +0300

    feat: implement PATCH method func

---
+++ src/utils/MethodPatch.js
@@ -0,0 +1,29 @@
+const { persons } = require('../data');
+const { ERROR } = require('../const');
+const errorHandler = require('./errorHandler')
+
+class MethodPatch {
+  updatePerson(request, response, id) {
+    request.on('data', chunk => {
+      let body;
+      try {
+        body = JSON.parse(chunk.toString());
+        const personIdx = persons.findIndex((person) => person.id === id);
+        if (personIdx !== -1) {
+          persons[personIdx].name = body.name || persons[personIdx].name;
+          persons[personIdx].age = body.age || persons[personIdx].age;
+          persons[personIdx].hobbies = [...(body.hobbies || persons[personIdx].hobbies)];
+          response.setHeader("Content-Type", "application/json");
+          response.statusCode = 200;
+          response.end(JSON.stringify(persons[personIdx]));
+        } else {
+          errorHandler(response, ERROR.PERSON_NOT_FOUND);
+        }
+      } catch {
+        errorHandler(response, ERROR.UNKNOWN);
+      }
+    });
+  }
+}
+
+module.exports = new MethodPatch();
