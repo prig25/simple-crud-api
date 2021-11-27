27b391c3dcad3570c256d7c615d962189a250de2
prig25 <5446068@gmail.com>
2021-11-27 20:19:59 +0300

    feat: implement POST method func

---
+++ src/utils/MethodPost.js
@@ -0,0 +1,34 @@
+const { v4: uuidv4 } = require('uuid');
+
+const { persons } = require('../data');
+const { ERROR } = require('../const');
+const errorHandler = require('./errorHandler')
+
+class MethodPost {
+  createPerson(request, response) {
+    request.on('data', chunk => {
+      let body;
+      try {
+        body = JSON.parse(chunk.toString());
+        if (body.name && body.age && body.hobbies) {
+          const person = {
+            id: uuidv4(),
+            name: body.name,
+            age: body.age,
+            hobbies: [...body.hobbies]
+          };
+          persons.push(person);
+          response.setHeader("Content-Type", "application/json");
+          response.statusCode = 201;
+          response.end(JSON.stringify(person));
+        } else {
+          errorHandler(response, ERROR.REQUIRED_FIELDS);
+        }
+      } catch {
+        errorHandler(response, ERROR.UNKNOWN);
+      }
+    });
+  }
+}
+
+module.exports = new MethodPost();
