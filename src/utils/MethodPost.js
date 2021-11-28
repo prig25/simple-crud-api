ad317be8b38127b109a40821a9cec03181f13c96
prig25 <5446068@gmail.com>
2021-11-28 21:02:00 +0300

    fix: add body checking

--- src/utils/MethodPost.js
+++ src/utils/MethodPost.js
@@ -6,28 +6,33 @@ const errorHandler = require('./errorHandler')
 
 class MethodPost {
   createPerson(request, response) {
-    request.on('data', chunk => {
-      let body;
-      try {
-        body = JSON.parse(chunk.toString());
-        if (body.name && body.age && body.hobbies) {
-          const person = {
-            id: uuidv4(),
-            name: body.name,
-            age: body.age,
-            hobbies: [...body.hobbies]
-          };
-          persons.push(person);
-          response.setHeader("Content-Type", "application/json");
-          response.statusCode = 201;
-          response.end(JSON.stringify(person));
-        } else {
-          errorHandler(response, ERROR.REQUIRED_FIELDS);
+    const isBody = request.headers['content-length'];
+    if (+isBody !== 0) {
+      request.on('data', chunk => {
+        let body;
+        try {
+          body = JSON.parse(chunk.toString());
+          if (body.name && body.age && body.hobbies) {
+            const person = {
+              id: uuidv4(),
+              name: body.name,
+              age: body.age,
+              hobbies: [...body.hobbies]
+            };
+            persons.push(person);
+            response.setHeader("Content-Type", "application/json");
+            response.statusCode = 201;
+            response.end(JSON.stringify(person));
+          } else {
+            errorHandler(response, ERROR.REQUIRED_FIELDS);
+          }
+        } catch {
+          errorHandler(response, ERROR.UNKNOWN);
         }
-      } catch {
-        errorHandler(response, ERROR.UNKNOWN);
-      }
-    });
+      });
+    } else {
+      errorHandler(response, ERROR.UNKNOWN);
+    }
   }
 }
 
--- src/utils/MethodPut.js
+++ src/utils/MethodPut.js
@@ -4,29 +4,34 @@ const errorHandler = require('./errorHandler')
 
 class MethodPut {
   replacePerson(request, response, id) {
-    request.on('data', chunk => {
-      let body;
-      try {
-        body = JSON.parse(chunk.toString());
-        const personIdx = persons.findIndex((person) => person.id === id);
-        if (personIdx !== -1) {
-          if (body.name && body.age && body.hobbies) {
-            persons[personIdx].name = body.name;
-            persons[personIdx].age = body.age;
-            persons[personIdx].hobbies = [...body.hobbies];
-            response.setHeader("Content-Type", "application/json");
-            response.statusCode = 200;
-            response.end(JSON.stringify(persons[personIdx]));
+    const isBody = request.headers['content-length'];
+    if (+isBody !== 0) {
+      request.on('data', chunk => {
+        let body;
+        try {
+          body = JSON.parse(chunk.toString());
+          const personIdx = persons.findIndex((person) => person.id === id);
+          if (personIdx !== -1) {
+            if (body.name && body.age && body.hobbies) {
+              persons[personIdx].name = body.name;
+              persons[personIdx].age = body.age;
+              persons[personIdx].hobbies = [...body.hobbies];
+              response.setHeader("Content-Type", "application/json");
+              response.statusCode = 200;
+              response.end(JSON.stringify(persons[personIdx]));
+            } else {
+              errorHandler(response, ERROR.REQUIRED_FIELDS);
+            }
           } else {
-            errorHandler(response, ERROR.REQUIRED_FIELDS);
+            errorHandler(response, ERROR.PERSON_NOT_FOUND);
           }
-        } else {
-          errorHandler(response, ERROR.PERSON_NOT_FOUND);
+        } catch {
+          errorHandler(response, ERROR.UNKNOWN);
         }
-      } catch {
-        errorHandler(response, ERROR.UNKNOWN);
-      }
-    });
+      });
+    } else {
+      errorHandler(response, ERROR.UNKNOWN);
+    }
   }
 }
 
